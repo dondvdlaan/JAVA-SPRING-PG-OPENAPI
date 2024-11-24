07cf8905f215199ef16b6e39120d959443d53f50
dondvdlaan <dondvdlaan@gmail.com>
2024-11-24 17:09:32 +0100

    DCM-000: Unit tests controller opschonen

--- src/main/java/dev/manyroads/matterreception/exception/MatterRequestEmptyOrNullException.java
+++ src/main/java/dev/manyroads/matterreception/exception/MatterRequestEmptyOrNullException.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value= HttpStatus.BAD_REQUEST)
-public final class CaseIDIsMissingException extends DCMException {
-    public CaseIDIsMissingException() {
-        super("DCM-003: CaseRequest CaseID is missing");
+public final class MatterRequestEmptyOrNullException extends DCMException {
+    public MatterRequestEmptyOrNullException() {
+        super("DCM-001: CaseRequest empty or Null");
     }
 }
