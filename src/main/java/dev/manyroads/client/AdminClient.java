01d0792507485dcca97059dd0793ee346a55460d
dondvdlaan <dondvdlaan@gmail.com>
2024-11-29 15:17:39 +0100

    DCM-010: Improved ExecInterrup Custmer Deceased

--- src/main/java/dev/manyroads/client/AdminClient.java
+++ src/main/java/dev/manyroads/client/AdminClient.java
@@ -1,14 +1,22 @@
 package dev.manyroads.client;
 
 import dev.manyroads.model.VehicleTypeEnum;
+import dev.manyroads.model.entity.Matter;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient("Decom-Admin")
 public interface AdminClient {
 
-    @GetMapping("/vehicles/{caseID}")
-    String searchVehicleType(@PathVariable("caseID") String caseID);
+    @GetMapping("/vehicles/{matterNr}")
+    String searchVehicleType(@PathVariable("matterNr") String matterNr);
+
+    @PostMapping("/terminate-matter")
+    String terminateMatter(@RequestBody Matter matter);
+
+
 
 }
