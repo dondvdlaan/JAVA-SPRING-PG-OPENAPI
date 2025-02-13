5611c241e3abb6f2c97d222905493e291ba84776
dondvdlaan <dondvdlaan@gmail.com>
2025-02-13 17:06:49 +0100

    DCM-027: Correction to Schema - Domain

--- src/main/java/dev/manyroads/model/entity/Charge.java
+++ src/main/java/dev/manyroads/model/entity/Charge.java
@@ -12,10 +12,17 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 import java.time.Instant;
-import java.util.*;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
 
 @Entity
 @Table(name = "charges")
